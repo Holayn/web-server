e4df2b66e16068160200b5da37cb7418de289463
Holayn <kwwong1597@gmail.com>
2022-07-06 21:04:57 -0400

    set csp for keepass

--- routes/keepass.js
+++ routes/keepass.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const router = express.Router();
-
+router.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+      childSrc: ["'self'", 'blob:'],
+      connectSrc: ["'self'", 'ws:', 'https:'],
+      defaultSrc: ["'self'"],
+      fontSrc: ['data:'],
+      formAction: ["'none'"],
+      imgSrc: ["'self'", 'data:', 'blob:', ' https://services.keeweb.info/'],
+      scriptSrc: ["'sha512-jYN26k9MdXnhqu7Ssy7r+y/87ND1Br7yx4NzRHwx8dSII1LvinqIRKaXoIb0ObI9HQFILRWBg5gPRszhILAC9g=='", "'unsafe-eval'"],
+      styleSrc: ["'sha512-63edYbLgq599MAsyyqgCBk18k1RCKpZ3FjZ9G3l0pQlvxVchNVaPhiAE+GwP9NKPZVIinJuNXJx2xY1tZkzvZg=='", 'blob:'],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+      workerSrc: ["'self'", 'blob:']
+    },
+  })
+);
 router.use('/', express.static(process.env.KEEPASS_PATH));
 
 module.exports = router;
