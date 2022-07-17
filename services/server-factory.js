d1f65a763211a5f13ea2f13fa268139431e5a2dd
Kai <kwwong1597@gmail.com>
2022-07-16 23:43:07 -0400

    fix: helmet before cors to ensure headers are always correct
    - OPTIONS requests would bypass helmet header handling

--- services/server-factory.js
+++ services/server-factory.js
@@ -9,8 +9,8 @@ module.exports = class ServerFactory {
   static createServer() {
     const app = express();
 
-    app.use(cors());
     app.use(helmet());
+    app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use((req, res, next) => {
